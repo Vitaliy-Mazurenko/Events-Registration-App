874b946be1018a51e469909a9f1b02f5c6aff464
Vitaliy <vit007m@gmail.com>
2024-05-19 23:54:33 +0300

    init: add frontend

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+app.use(express.json());
+const dataBase = require('./data.json');
+const PORT = process.env.PORT || 4000;
+const fs = require('fs');
+
+app.use(cors());
+
+app.get('/', (req, res)=> {
+    res.end('Server listening on 4000');
+});
+
+app.get('/api', (req, res)=> {
+    res.json({ data: dataBase });
+});
+
+app.post('/add',  (req, res)=> {
+  const receivedData = JSON.stringify(req.body);
+  
+  fs.writeFile( './data.json', receivedData, {encoding: 'utf8', flag: 'a'}, (err) => {
+    if( err ) {
+      next({message: 'Failed to create file', status: 500})
+      return;
+    }
+  })
+  res.end("{}");
+});
+
+app.listen(PORT, () =>{
+    console.log(`Server is working on port ${PORT}`);
+});
+
+
